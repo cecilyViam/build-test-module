03bea718b8b9f4fb697bec8c21e58c13c1fee7a2
Cecily Munn <cecily.munn@viam.com>
2026-05-18 15:49:56 -0400

    Add stub generic module and build config

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"context"
+
+	"go.viam.com/rdk/components/generic"
+	"go.viam.com/rdk/logging"
+	"go.viam.com/rdk/module"
+	"go.viam.com/rdk/resource"
+)
+
+var model = resource.NewModel("cecilyViam", "build-test", "stub")
 
 func main() {
-	fmt.Println("build-test-module")
+	resource.RegisterComponent(generic.API, model, resource.Registration[resource.Resource, resource.NoNativeConfig]{
+		Constructor: newStub,
+	})
+	module.ModularMain(resource.APIModel{generic.API, model})
+}
+
+type stub struct {
+	resource.Named
+	resource.AlwaysRebuild
+	resource.TriviallyCloseable
+}
+
+func newStub(_ context.Context, _ resource.Dependencies, conf resource.Config, _ logging.Logger) (resource.Resource, error) {
+	return &stub{Named: conf.ResourceName().AsNamed()}, nil
+}
+
+func (s *stub) DoCommand(_ context.Context, cmd map[string]interface{}) (map[string]interface{}, error) {
+	return cmd, nil
 }
